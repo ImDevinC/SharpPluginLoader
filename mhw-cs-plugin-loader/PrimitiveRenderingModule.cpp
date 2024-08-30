8292fca00a4e6a736cd8ccbbab43420e910aaa7e
Devin Collins <me@imdevinc.com>
2024-08-30 09:41:24 -0700

    testing

--- mhw-cs-plugin-loader/PrimitiveRenderingModule.cpp
+++ mhw-cs-plugin-loader/PrimitiveRenderingModule.cpp
@@ -826,7 +826,8 @@ void PrimitiveRenderingModule::late_init_d3d11(D3DModule* d3dmodule) {
             blob.GetAddressOf(),
             error_blob.GetAddressOf()
          );
-        HandleResultMsg(result, (char *)error_blob->GetBufferPointer());
+        //HandleResultMsg(result, (char *)error_blob->GetBufferPointer());
+        HandleResult(result);
     };
 
     load(vs, "vs_5_0", vs_blob);
@@ -1105,7 +1106,7 @@ void PrimitiveRenderingModule::late_init_d3d12(D3DModule* d3dmodule, IDXGISwapCh
             blob.GetAddressOf(),
             error_blob.GetAddressOf()
         );
-        HandleResultMsg(result, (char*)error_blob->GetBufferPointer());
+        HandleResult(result);
     };
 
     load(vs, "vs_5_0", vs_blob);
